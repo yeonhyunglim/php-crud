75964a967cbd55772750f777c242466a438dc465
Thelma_Lim <dusgud711@ewhain.net>
2022-03-31 19:52:21 +0900

    DockerでCRUD練習用のPHP環境構築しました。

---
+++ html/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP CRUD</title>
+        <script src="https://code.jquery.com/jquery-2.1.3.min.js"><script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    </head>
+
+
+    <body>
+        <form action="" method="POST">
+            <label>Name</label>
+            <input type="text" name="name" value="Enter your name">
+            <label>Location</label>
+            <input type="text" name="location" value="Enter your location">
+            <button type="submit" name="save">Save</button>
+            </form>
+    </body>
+</html>
