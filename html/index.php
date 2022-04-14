2d241982352d96127dfd0c1bf8437cd852f8cc28
Thelma_Lim <dusgud711@ewhain.net>
2022-04-14 19:16:36 +0900

    Edit機能を実装しました。

--- html/index.php
+++ html/index.php
@@ -65,17 +65,26 @@
         
         <div class="row justify-content-center">
         <form action="process.php" method="POST">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
             <div class="form-group">
             <label>Name</label>
-            <input type="text" name="name" class ="form-control" value="Enter your name">
+            <input type="text" name="name" class="form-control"
+                   value="<?php echo $name; ?>" placeholder="Enter your name">
             </div>
             <div class="form-group">
             <label>Location</label>
-            <input type="text" name="location" class="form-control" value="Enter your location">
+            <input type="text" name="location" class="form-control" 
+                   value="<?php echo $location; ?>" placeholder="Enter your location">
             </div>
             <div class="form-group">
+            <?php
+            if ($update == true):   
+            ?>
+            <button type="submit" class="btn btn-primary" name="update">Update</button>
+            <?php else: ?>
             <button type="submit" class="btn btn-primary" name="save">Save</button>
-            </div>
+            <?php endif; ?>
+        </div>
         </form>
         </div>
         </div>
--- html/process.php
+++ html/process.php
@@ -3,6 +3,11 @@ session_start();
 
 $mysqli = new mysqli("mysql:3306", 'crud', 'crud', 'crud') or die(mysqli_error($mysqli));
 
+$id = 0;
+$update = false;
+$name = '';
+$location = "";
+
 
 if (isset($_POST['save'])){
     $name = $_POST['name'];
@@ -13,7 +18,7 @@ if (isset($_POST['save'])){
     $_SESSION['message'] = "Record has been saved!";
     $_SESSION['msg_type'] = "success";
     
-    header("location: ./index.php");
+    header("location: index.php");
 }
 
 if (isset($_GET['delete'])){
@@ -23,5 +28,32 @@ if (isset($_GET['delete'])){
     $_SESSION['message'] = "Record has been deleted!";
     $_SESSION['msg_type'] = "danger";
 
-    header("location: ./index.php");
+    header("location: index.php");
+}
+
+if (isset($_GET['edit'])){
+    $id = $_GET['edit'];
+    $update = true;
+    $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error());
+
+
+    if ((mysqli_num_rows($result)) == 1){ // count($result) made an error like Warning: count(): Parameter must be an array or an object that implements Countable
+        $row = $result->fetch_array();
+        $name = $row['name'];
+        $location = $row['location'];
+    }  
+}
+
+if (isset($_POST['update'])){
+
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+
+    $mysqli->query("UPDATE data SET name='$name', location='$location' WHERE id=$id") or die($mysqli->error);
+
+    $_SESSION['message'] = "Record has been updated!";
+    $_SESSION['msg_type'] = "warning";
+
+    header("location: index.php");
 }
